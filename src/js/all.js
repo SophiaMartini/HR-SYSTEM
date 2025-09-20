6afac9fdcec5e5fc21d73bfe3ddb6ca8e6c30d32
Sophia Martini <sopmartinidea@gmail.com>
2025-09-20 15:33:29 -0300

    usando fetch pra puxar a navbar de cada tela

--- src/js/all.js
+++ src/js/all.js
@@ -1,3 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const target = document.querySelector("header[id^='header-']");
+  if (!target) return;
+
+  const headerType = target.id;
+
+  fetch('/src/components/footer-header.html')
+    .then(response => {
+      if (!response.ok) throw new Error("Erro ao carregar header");
+      return response.text();
+    })
+    .then(data => {
+      const tempDiv = document.createElement("div");
+      tempDiv.innerHTML = data;
+
+      const selectedHeader = tempDiv.querySelector(`#${headerType}`);
+      if (selectedHeader) {
+        target.innerHTML = selectedHeader.innerHTML;
+      }
+    })
+    .catch(error => console.error(error));
+});
+
+
+  //tentano arrumar essa bosta de menu mobile
 $(document).ready(function( ){
     $('#mobile_bt').on('click', function(){
         $('#mobile_menu').toggleClass('active');
